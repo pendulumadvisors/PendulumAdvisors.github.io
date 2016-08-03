73873a00eac7d36240965e3e681b9771ab7fadc9
Jodie Ryu <jodie.ryu@gmail.com>
2016-08-03 22:16:49 +0200

    adding stuff to repo

---
+++ script.js
@@ -0,0 +1,260 @@
+var app = angular.module("pendulumApp", ["ngRoute", "firebase"]); 
+
+// app.run(["$rootScope", "$location", function($rootScope, $location) {
+//   $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+//     // We can catch the error thrown when the $requireSignIn promise is rejected
+//     // and redirect the user back to the home page
+//     if (error === "AUTH_REQUIRED") {
+//       $location.path("/");
+//     }
+//   });
+// }]);
+
+app.config(function($routeProvider, $locationProvider) {
+	$routeProvider.when("/", {
+		// controller: 'HomeCtrl',
+		templateUrl: "templates/home.html",
+		controller: 'ScrollCtrl'
+    });
+	$routeProvider.when("/login", {
+		templateUrl: "templates/login.html",
+		controller: "LoginCtrl"
+	});
+	$routeProvider.when("/research", {
+		templateUrl: "templates/research.html"
+	});
+	$routeProvider.when('/main', {
+		templateUrl: 'templates/main.html',
+		controller: 'MainCtrl'
+		// resolve: {
+  //     		"currentAuth": function($firebaseAuth) {
+	 //        return $firebaseAuth().$requireSignIn();
+  //     		}
+  // 		}	
+	});
+	$routeProvider.when('/InternalResearch', {
+		templateUrl: 'templates/InternalResearch.html'
+	});
+	$routeProvider.when('/ExternalResearch', {
+		templateUrl: 'templates/ExternalResearch.html'
+	});
+	$routeProvider.when('/podcasts', {
+		templateUrl: 'templates/podcasts.html' 
+	});
+	$routeProvider.when('/favoriteBooks', {
+		templateUrl: 'templates/favoriteBooks.html'
+	});
+	$routeProvider.when('/favoriteArticles', {
+		templateUrl: 'templates/favoriteArticles.html'
+	});
+});
+
+app.controller('LoginCtrl', function($scope, $firebaseAuth, $location) {
+    $scope.authObj = $firebaseAuth();
+
+	$scope.login = function() {
+		console.log("do login");
+		$scope.errorMessage = "";
+		console.log($scope.password);
+		
+		var ref = new Firebase("https://pendulumadvisors-f7655.firebaseio.com");
+		ref.authWithPassword({
+			"password": "123456"
+		}, function(error, authData) {
+			if (error) {
+				console.log("Login Failed!", error);
+			} else {
+				console.log("Authentication success!", authData);
+			}
+		});
+		// $scope.authObj.$signInWithEmailAndPassword($scope.email, $scope.password)
+		// .then(function(firebaseUser) {
+		// 	console.log("Signed in as:", firebaseUser.uid);
+		// 	$scope.firebaseUser1 = firebaseUser;
+		// 	console.log($scope.firebaseUser1);
+		// }).catch(function(error) {
+		// 	console.error("Authentication failed:", error);
+		// 	$scope.errorMessage = error.message;
+		// });
+	
+	}
+});
+
+// /* Scroll down button function */
+// $(function() {
+// 	$('a[href*=#]').on('click', function(e) {
+// 		e.preventDefault();
+// 		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+// 	});
+// });
+
+app.directive('fileDownload',function(){
+    return{
+        restrict:'A',
+        scope:{
+            fileDownload:'=',
+            fileName:'=',
+        },
+
+        link:function(scope,elem,atrs){
+
+
+            scope.$watch('fileDownload',function(newValue, oldValue){
+
+                if(newValue!=undefined && newValue!=null){
+                    console.debug('Downloading a new file'); 
+                    var isFirefox = typeof InstallTrigger !== 'undefined';
+                    var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+                    var isIE = /*@cc_on!@*/false || !!document.documentMode;
+                    var isEdge = !isIE && !!window.StyleMedia;
+                    var isChrome = !!window.chrome && !!window.chrome.webstore;
+                    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+                    var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+                    if(isFirefox || isIE || isChrome){
+                        if(isChrome){
+                            console.log('Manage Google Chrome download');
+                            var url = window.URL || window.webkitURL;
+                            var fileURL = url.createObjectURL(scope.fileDownload);
+                            var downloadLink = angular.element('<a></a>');//create a new  <a> tag element
+                            downloadLink.attr('href',fileURL);
+                            downloadLink.attr('download',scope.fileName);
+                            downloadLink.attr('target','_self');
+                            downloadLink[0].click();//call click function
+                            url.revokeObjectURL(fileURL);//revoke the object from URL
+                        }
+                        if(isIE){
+                            console.log('Manage IE download>10');
+                            window.navigator.msSaveOrOpenBlob(scope.fileDownload,scope.fileName); 
+                        }
+                        if(isFirefox){
+                            console.log('Manage Mozilla Firefox download');
+                            var url = window.URL || window.webkitURL;
+                            var fileURL = url.createObjectURL(scope.fileDownload);
+                            var a=elem[0];//recover the <a> tag from directive
+                            a.href=fileURL;
+                            a.download=scope.fileName;
+                            a.target='_self';
+                            a.click();//we call click function
+                        }
+
+
+                    }else{
+                        alert('SORRY YOUR BROWSER IS NOT COMPATIBLE');
+                    }
+                }
+            });
+
+        }
+    }
+})
+app.controller('MainCtrl', function($scope, $firebaseAuth, $location, anchorSmoothScroll) {
+    var auth = $firebaseAuth();
+    $scope.logout = function() {
+    	auth.$signOut();
+    	$location.path("/");
+  	};
+	// $scope.download = function() {
+	// 	final long ONE_MEGABYTE = 1024 * 1024;
+	// 	islandRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+ //   			@Override
+ //    		public void onSuccess(byte[] bytes) {
+ //        		// Data for "images/island.jpg" is returns, use this as needed
+ //    		}
+	// 	}).addOnFailureListener(new OnFailureListener() {
+ //    		@Override
+ //    		public void onFailure(@NonNull Exception exception) {
+ //        		// Handle any errors
+ //    		}
+	// 	});
+	// };
+	$scope.myBlobObject=undefined;
+	$scope.getFile=function(){
+        console.log('download started, you can show a wating animation');
+        serviceAsPromise.getStream({param1:'data1',param1:'data2'})
+        .then(function(data){//is important that the data was returned as Aray Buffer
+                console.log('Stream download complete, stop animation!');
+                $scope.myBlobObject=new Blob([data],{ type:'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
+        },function(fail){
+                console.log('Download Error, stop animation and show error message');
+                                    $scope.myBlobObject=[];
+                                });
+                            };
+
+    $scope.gotoElement = function (eID){
+      // set the location.hash to the id of
+      // the element you wish to scroll to.
+      $location.hash('bottom');
+ 		console.log("hey");
+      // call $anchorScroll()
+      anchorSmoothScroll.scrollTo(eID);
+    };
+
+});
+
+app.service('anchorSmoothScroll', function(){
+    
+    this.scrollTo = function(eID) {
+
+        // This scrolling function 
+        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+        
+        var startY = currentYPosition();
+        var stopY = elmYPosition(eID);
+        var distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        var speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        var step = Math.round(distance / 25);
+        var leapY = stopY > startY ? startY + step : startY - step;
+        var timer = 0;
+        if (stopY > startY) {
+            for ( var i=startY; i<stopY; i+=step ) {
+                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for ( var i=startY; i>stopY; i-=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+        
+        function currentYPosition() {
+            // Firefox, Chrome, Opera, Safari
+            if (self.pageYOffset) return self.pageYOffset;
+            // Internet Explorer 6 - standards mode
+            if (document.documentElement && document.documentElement.scrollTop)
+                return document.documentElement.scrollTop;
+            // Internet Explorer 6, 7 and 8
+            if (document.body.scrollTop) return document.body.scrollTop;
+            return 0;
+        }
+        
+        function elmYPosition(eID) {
+            var elm = document.getElementById(eID);
+            var y = elm.offsetTop;
+            var node = elm;
+            while (node.offsetParent && node.offsetParent != document.body) {
+                node = node.offsetParent;
+                y += node.offsetTop;
+            } return y;
+        }
+
+    };
+    
+});
+
+app.controller('ScrollCtrl', function($scope, $location, anchorSmoothScroll) {
+    $scope.gotoElement = function (eID){
+      // set the location.hash to the id of
+      // the element you wish to scroll to.
+      console.log("in function");
+      $location.hash('bottom');
+ 
+      // call $anchorScroll()
+      anchorSmoothScroll.scrollTo(eID);
+      
+    };
+  });
